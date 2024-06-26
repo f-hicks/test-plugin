b237f308dda2fce92f339ca6839125d78f6c28ae
f-hicks <84518177+f-hicks@users.noreply.github.com>
2024-06-26 12:30:38 +0100

    add methods for experience gain and player damage

--- src/main/java/me/fhicks/testplugin/EventListener.java
+++ src/main/java/me/fhicks/testplugin/EventListener.java
@@ -1,9 +1,11 @@
 package me.fhicks.testplugin;
 
-import io.github.cdimascio.dotenv.Dotenv;
+import com.destroystokyo.paper.event.player.PlayerPickupExperienceEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -12,6 +14,8 @@ import java.io.IOException;
 
 
 public class EventListener implements Listener { //Implements the Listener interface
+
+    // TODO: check which player each event is for, and potentially send a api request to a different server for each player.
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
@@ -24,4 +28,17 @@ public class EventListener implements Listener { //Implements the Listener inter
         HomeAssistantAPI.sendRequest("/api/services/script/turn_on", "script.mc_death");
     }
 
+    @EventHandler
+    public void onPlayerXP(PlayerPickupExperienceEvent event) throws IOException {
+        HomeAssistantAPI.sendRequest("/api/services/script/turn_on", "script.mc_xp");
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageEvent event) throws IOException {
+        if (event.getEntity() instanceof Player){ // check if it is a player being damaged
+            //currently still shows this if the damage is blocked e.g. by a shield
+            HomeAssistantAPI.sendRequest("/api/services/script/turn_on", "script.mc_damage");
+
+        }
+    }
 }
